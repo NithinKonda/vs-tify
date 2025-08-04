af7938262cd305183c424d4d385be82bcc1bd176
NithinKonda <nithinkonda142@gmail.com>
2025-08-05 00:10:11 +0530

    opening panel

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Create status bar item
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  statusBarItem.text = '$(play-circle) Spotify';
+  statusBarItem.command = 'spotify-personal.openSpotifyPanel';
+  statusBarItem.show();
+  context.subscriptions.push(statusBarItem);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "spotify-personal" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('spotify-personal.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from spotify-personal!');
-	});
-
-	context.subscriptions.push(disposable);
+  // Register command to open webview
+  context.subscriptions.push(
+    vscode.commands.registerCommand('spotify-personal.openSpotifyPanel', () => {
+      const panel = vscode.window.createWebviewPanel(
+        'spotifyPanel',
+        'Spotify',
+        vscode.ViewColumn.One,
+        { enableScripts: true }
+      );
+      panel.webview.html = getWebviewContent(); // We'll define this function later
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+// Placeholder for webview HTML
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head><title>Spotify Panel</title></head>
+  <body><h1>Spotify Integration</h1></body>
+  </html>`;
+}
